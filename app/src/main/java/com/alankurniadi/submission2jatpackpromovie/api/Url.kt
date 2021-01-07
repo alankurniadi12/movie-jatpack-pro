57c8a3f86b183664d2dc1ed4e840ab52e59d7c2e
alankurniadi12 <alankurniadi53@gmail.com>
2021-01-07 08:18:42 +0700

    api key censored

--- app/src/main/java/com/alankurniadi/submission2jatpackpromovie/api/Url.kt
+++ app/src/main/java/com/alankurniadi/submission2jatpackpromovie/api/Url.kt
@@ -1,7 +1,7 @@
 package com.alankurniadi.submission2jatpackpromovie.api
 
 object Url {
-    const val API_KEY = "54f1a575ff34a72f82134bf90ea5ff4f"
+    const val API_KEY = "YOUR_API_KEY"
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val POSTER_URL = "https://image.tmdb.org/t/p/w185"
     const val BACKDROP_URL = "https://image.tmdb.org/t/p/w780"
